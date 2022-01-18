4c823007cb3b0de045a84c839636dda237ac55b4
Gmail <pitapat7676@gmail.com>
2022-01-18 17:56:10 +0900

    mainPage 01-18

--- js/main.js
+++ js/main.js
@@ -9,7 +9,27 @@ $(".gnb>li").hover(function(){
 
 
 /* sec02 click */
- $(".Images_click>ul>li>img").click(function(){
+ $(".Images_click>div>img").click(function(){
     let imgSrc = $(this).attr("src");
     $(".Img_big>img").attr("src", imgSrc);
 })
+
+
+
+
+/* scroll event header */
+
+$(window).scroll(function(){
+    let scroll_top = $(this).scrollTop();
+
+    if(scroll_top <= 300){
+        $("#header").css({"background":"#fff"})
+        $(".logo>img").css({"filter":"none"})
+        $(".gnb>li>a").css({"color":"var(--main-color)"})
+    }
+    if(scroll_top >= 300){
+        $("#header").css({"background":"green"})
+        $(".logo>img").css({"filter":"brightness(0) invert(1)"})
+        $(".gnb>li>a").css({"color":"var(--color-white)"})
+    }
+});
