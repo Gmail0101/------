b484fa22499944c8be6dcf1d0730380c20782502
Gmail <pitapat7676@gmail.com>
2022-01-21 09:58:04 +0900

    0120 camp

--- js/main.js
+++ js/main.js
@@ -6,12 +6,44 @@ $(".gnb>li").hover(function(){
     $(this).children("ul").stop().slideToggle(300)
 });
 
+$(".menu-list").click(function(){
+    if($(this).hasClass("off")){
+        $("#header").css({"height":"auto"})     /* header auto 해서 세로크기를 자동으로 */
+        $(".gnb").css({
+            "float":"none",   /* float 지움 */
+            "display":"block",  /* show  */
+        })
+        $(".gnb>li").css({
+            "float":"none",   /* float 지움 */
+            "width":"100%"  
+        })
+        $(this).css({"display":"block"});
+        $(this).removeClass("off");
+        $(this).addClass("active");
+    } else if($(this).hasClass("active")){
+        $("#header").css({"height":"10vh"})
+        $(".gnb").css({
+            "float":"right",
+            "display":"none",
+        })
+        $(".gnb>li").css({
+            "float":"left",
+            "width":"130px"  
+        })
+        $(this).css({"display":"none"});
+        $(this).removeClass("active");
+        $(this).addClass("off");
+    }
+})
+
+
+
 
 
 /* sec02 click */
  $(".Images_click>div>img").click(function(){
-    let imgSrc = $(this).attr("src");
-    $(".Img_big>img").attr("src", imgSrc);
+    let imgSrc = $(this).attr("src");   /* 내 주소 읽어옴 */
+    $(".Img_big>img").attr("src", imgSrc);  /* 읽어온 주소를 다른 img src 에 대입 */
 })
 
 
@@ -20,7 +52,7 @@ $(".gnb>li").hover(function(){
 /* scroll event header */
 
 $(window).scroll(function(){
-    let scroll_top = $(this).scrollTop();
+    let scroll_top = $(this).scrollTop();   /* window 스크롤 */
 
     if(scroll_top <= 300){
         $("#header").css({"background":"#fff"})
